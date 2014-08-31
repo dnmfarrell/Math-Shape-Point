d1dfd7f52945bd1df31ec60cf8e70b8c32fadbf3
sillymoose <davidnmfarrell@gmail.com>
2014-08-30 22:16:09 -0400

    Changed method names, new tests

--- cpanfile
+++ cpanfile
@@ -5,6 +5,7 @@ requires "strict" => "0";
 requires "warnings" => "0";
 
 on 'test' => sub {
+  requires "Test::Exception" => "0";
   requires "Test::More" => "0";
   requires "perl" => "5.006";
 };
--- lib/Math/Shape/Point.pm
+++ lib/Math/Shape/Point.pm
@@ -20,11 +20,11 @@ This module is designed to provide some useful 2d functions for manipulating poi
     use Math::Shape::Point;
     use Math::Trig ':pi';
 
-    my $p0 = Math::Shape::Point->new(0, 0, 0);
-    my $p1 = Math::Shape::Point->new(5, 5, 0);
-    $p0->rotateAboutPoint($p1, pip2);
-    my $angle = $p0->getAngleToPoint($p1);
-    my $distance = $p0->getDistanceToPoint($p1);
+    my $p1 = Math::Shape::Point->new(0, 0, 0);
+    my $p2 = Math::Shape::Point->new(5, 5, 0);
+    $p1->rotate_about_point($p2, pip2);
+    my $angle = $p1->get_angle_to_point($p2);
+    my $distance = $p1->get_distance_to_point($p2);
 
 =head1 METHODS
 
@@ -32,9 +32,12 @@ This module is designed to provide some useful 2d functions for manipulating poi
 
 Instantiates a new point object. Requires the x and y cartesian coordinates and the facing direction in radians.
 
+    my $p = Math::Shape::Point->new(0, 0, 0);
+
 =cut
 
 sub new {
+    croak 'Incorrect number of arguments for new()' unless @_ == 4;
     my ($class, $x, $y, $r) = @_;
     my $self = { 
         x => $x,
@@ -44,52 +47,62 @@ sub new {
     return bless $self, $class;
 }
 
-=head2 getLocation
+=head2 get_location
 
 Returns an arrayref containing the point's location in cartesian coordinates.
 
+    $p->get_location;
+
 =cut
 
-sub getLocation { [$_[0]->{x}, $_[0]->{y}] }
+sub get_location { [$_[0]->{x}, $_[0]->{y}] }
 
 
-=head2 setLocation
+=head2 set_location
 
 Sets the point's location in cartesian coordinates. Requires two numbers as inputs for the x and y location.
 
+    $p->set_location($x, $y);
+
 =cut
 
-sub setLocation {
+sub set_location {
     my ($self, $x, $y) = @_;
     $self->{x} = $x;
     $self->{y} = $y;
     1;
 }
 
-=head2 getDirection
+=head2 get_direction
 
 Returns the current facing direction in radians.
 
+    $p->get_direction;
+
 =cut
 
-sub getDirection {
+sub get_direction {
     return $_[0]->{r};
 }
 
-=head2 setDirection
+=head2 set_direction
 
 Sets the current facing direction in radians.
 
+    $p->set_direction(2.5);
+
 =cut
 
-sub setDirection {
-    $_[0]->{r} = $_[0]->normalizeRadian($_[1]);
+sub set_direction {
+    $_[0]->{r} = $_[0]->normalize_radian($_[1]);
     1;
 }
 
 =head2 advance
 
 Requires a numeric distance argument - moves the point forward that distance in Cartesian coordinates towards the direction it is facing.
+
+    $p->advance(5);
 
 =cut
 
@@ -103,50 +116,60 @@ sub advance {
 
 Updates the point's facing direction by radians.
 
+    $p->rotate(2);
+
 =cut
 
 sub rotate {
-    $_[0]->{r} = $_[0]->{r} + $_[0]->normalizeRadian($_[1]);
+    $_[0]->{r} = $_[0]->{r} + $_[0]->normalize_radian($_[1]);
     1;
 }
 
-=head2 rotateAboutPoint
+=head2 rotate_about_point
 
 Rotates the point around another point of origin. Requires a point object and the angle in radians to rotate. This method updates the facing direction of the point object, as well as it's location.
 
+    my $p1 = Math::Shape::Point->new(0, 0, 0); #new point at 0,0 facing 0 radians.
+    my $p2 = Math::Shape::Point->new(1, 2, 3.14); #new point at 1,2 facing 3.14 radians.
+    $p1->rotate_about_point($p2, 3.14);
+
 =cut
 
-sub rotateAboutPoint {
+sub rotate_about_point {
     my ($self, $origin, $r) = @_;
-    $r = $self->normalizeRadian($r);
+    $r = $self->normalize_radian($r);
     $self->{x} = $origin->{x} + int(cos($r) * ($self->{x} - $origin->{x}) - sin($r) * ($self->{y} - $origin->{y}));
     $self->{y} = $origin->{y} + int(sin($r) * ($self->{x} - $origin->{x}) + cos($r) * ($self->{y} - $origin->{y}));
     $self->rotate($r);
     1;
 }
 
-=head2 getDistanceToPoint
+=head2 get_distance_to_point
 
 Returns the distance to another point object. Requires a point object as an argument.
 
+    $p1->get_distance_to_point($p2);
+
 =cut
 
-sub getDistanceToPoint {
+sub get_distance_to_point {
     sqrt ( abs($_[0]->{x} - $_[1]->{x}) ** 2 + abs($_[0]->{y} - $_[1]->{y}) ** 2);
 }
 
-=head2 getAngleToPoint
+=head2 get_angle_to_point
 
 Returns the angle of another point object. Requires a point as an argument.
 
+    $p1->get_angle_to_point($p2);
+
 =cut
 
-sub getAngleToPoint {
+sub get_angle_to_point {
     my ($self, $p) = @_;
 
     # check points are not at the same location
-    if ($self->getLocation->[0] == $p->getLocation->[0]
-        && $self->getLocation->[1] == $p->getLocation->[1]) 
+    if ($self->get_location->[0] == $p->get_location->[0]
+        && $self->get_location->[1] == $p->get_location->[1]) 
     {
         croak 'Error: points are at the same location';
     }
@@ -154,38 +177,40 @@ sub getAngleToPoint {
     my $atan = atan2($p->{y} - $self->{y}, $p->{x} - $self->{x});
 
     if ($atan <= 0) { # lower half
-        return abs($atan) + pip2 + $self->getDirection;
+        return abs($atan) + pip2 + $self->get_direction;
     }
     elsif ($atan <= pip2)  { # upper right quadrant
-        return abs($atan - pip2) + $self->getDirection;
+        return abs($atan - pip2) + $self->get_direction;
     }
     else { # upper left quadrant
-        return pi2 - $atan + pip2 + $self->getDirection;
+        return pi2 - $atan + pip2 + $self->get_direction;
     }
 }
 
-=head2 getDirectionToPoint
+=head2 get_direction_to_point
 
 Returns the direction of another point objection as a string (front, right, back or left). Assumes a 90 degree angle per direction.  Requires a point object as an argument.
 
+    $p1->get_direction_to_point($p2); # front
+
 =cut
 
-sub getDirectionToPoint {
+sub get_direction_to_point {
     my ($self, $p) = @_;
-    my $angle = $self->getAngleToPoint($p);
+    my $angle = $self->get_angle_to_point($p);
     if    ($angle > 0 - pip4  && $angle <= pip4)      { return 'front' }
     elsif ($angle > pip4      && $angle <= pi - pip4) { return 'right' }
     elsif ($angle > pi - pip4 && $angle <= pi + pip4) { return 'back'  }
     return 'left';
 }
 
-=head2 normalizeRadian
+=head2 normalize_radian
 
 Takes a radian argument and returns it between 0 and PI2. Negative numbers are assumed to be backwards (e.g. -1.57 == PI + PI / 2)
 
 =cut
 
-sub normalizeRadian {
+sub normalize_radian {
     my ($self, $radians) = @_;
     my $piDecimal = ($radians / pi2 - int($radians / pi2));
     return $piDecimal < 0 ? pi2 + $piDecimal * pi2 : $piDecimal * pi2;
@@ -200,7 +225,7 @@ David Farrell, C<< <davidnmfarrell at gmail.com> >>, L<perltricks.com|http://per
 =head1 BUGS
 
 Please report any bugs or feature requests to C<bug-math-shape-point at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=math-shape-point>.  I will be notified, and then you'll automatically be notified of 
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=math-shape-point>.  I will be notified, and then you'll automatically be notified of
 progress on your bug as I make changes.
 
 =head1 SUPPORT
